03f683263fb58f72dc979a25e846d9a4879312df
moluoxixi <10811655+moluoxixi@user.noreply.gitee.com>
2024-10-31 19:42:46 +0800

    init

---
+++ src/components/Calendar/Calendar.stories.ts
@@ -0,0 +1,28 @@
+import type {Meta, StoryObj} from '@storybook/react';
+// import {fn} from '@storybook/test';
+
+import Calendar from './index.tsx';
+
+const meta = {
+    title: '日历',
+    component: Calendar,
+    parameters: {
+			docs: {
+				description: {
+					defaultValue: '测试',
+				}
+			},
+    },
+    tags: ['!autodocs'],
+    argTypes: {
+    },
+    args: {},
+} satisfies Meta<typeof Calendar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+    },
+};
