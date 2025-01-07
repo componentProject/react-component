40d8c15f04ee146ef13f1d5ba8478fe0d6265355
moluoxixi <10811655+moluoxixi@user.noreply.gitee.com>
2025-01-07 17:40:03 +0800

    feat: 规范代码

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,10 @@
 export default {
-	extends: ["stylelint-config-recommended", "stylelint-config-tailwindcss"],
+	extends: [
+		"stylelint-config-standard",
+		"stylelint-config-standard-scss",
+		"stylelint-config-standard-less",
+		"stylelint-config-tailwindcss",
+	],
 	rules: {
 		// 在这里可以自定义的规则，覆盖默认的规则
 	},
