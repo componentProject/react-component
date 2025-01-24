4c4ed36ba7748b0b7e7d8b48f54b7bbb239a83df
wanglun <wrnm.@.com>
2025-01-24 10:39:43 +0800

    refactor: 重构stories结构

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,12 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: [ "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-		"@storybook/addon-storysource"
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
+	stories: ["../.stories/*.stories.@(js|jsx|mjs|ts|tsx)", "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-storysource"],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
 	docs: {
 		autodocs: true,
 	},
