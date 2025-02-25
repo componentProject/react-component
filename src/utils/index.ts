25586c79da39dc39aaa617baa60e66b976ffcef2
wanglun <wrnm.@.com>
2025-02-25 11:02:26 +0800

    fix: ts bug

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,40 @@ export function getId(messageProps: { id?: number } = {}) {
 	count += 1;
 	return count;
 }
+
+export function cloneDeep(obj: any, map = new WeakMap()) {
+	if (typeof obj !== "object" || obj === null) {
+		return obj;
+	}
+
+	// 如果对象已经被拷贝过，直接返回缓存中的拷贝
+	if (map.has(obj)) {
+		return map.get(obj);
+	}
+
+	let clone: any;
+
+	// 判断是数组还是对象
+	if (Array.isArray(obj)) {
+		clone = [];
+		map.set(obj, clone);
+		obj.forEach((item, index) => {
+			clone[index] = cloneDeep(item, map);
+		});
+	} else {
+		clone = {};
+		map.set(obj, clone);
+		for (const key in obj) {
+			if (Object.prototype.hasOwnProperty.call(obj, key)) {
+				clone[key] = cloneDeep(obj[key], map);
+			}
+		}
+	}
+
+	// 处理函数类型
+	if (typeof obj === "function") {
+		clone[typeof obj] = obj.toString();
+	}
+
+	return clone;
+}
