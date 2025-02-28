bc57fe78c3e8610202d127bf8d10cd57b791c7bd
moluoxixi <1983531544@qq.com>
2025-02-28 12:47:01 +0800

    perf: 合并vite.config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,12 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
+import type { PluginOption } from "vite";
 import viteConfig from "../vite.config";
 import type { modeType } from "../vite.config";
+
+type PluginOptionType = PluginOption & {
+	name?: string;
+};
 const config: StorybookConfig = {
 	stories: ["./.stories/*.stories.@(js|jsx|mjs|ts|tsx)", "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-storysource"],
@@ -14,10 +19,14 @@ const config: StorybookConfig = {
 	},
 	async viteFinal(config, { configType }) {
 		const { plugins, build } = viteConfig({ mode: configType, type: "storybook" } as modeType);
-		return mergeConfig(config, {
+		const mergeconfig = mergeConfig(config, {
 			build,
 			plugins,
 		});
+		mergeconfig.plugins = mergeconfig.plugins.filter((plugin: PluginOptionType) => {
+			return plugin?.name != "vite-plugin-cdn-import";
+		});
+		return config;
 	},
 	docs: {
 		autodocs: true,
